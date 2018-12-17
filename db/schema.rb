b581bdb4e8cc75f0b1bbc7b6ba618d4265ca9be2
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-12-17 10:33:50 +0100

    Product model is added

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_091814) do
+ActiveRecord::Schema.define(version: 2018_12_17_093121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "sku"
+    t.string "description"
+    t.integer "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
