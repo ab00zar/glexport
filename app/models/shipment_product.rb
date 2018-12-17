8cc663c4e4e3bf8dbb7ecbde6b22a62f190e0e04
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-12-17 10:39:48 +0100

    Shipment_product model is added

---
+++ app/models/shipment_product.rb
@@ -0,0 +1,2 @@
+class ShipmentProduct < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_093121) do
+ActiveRecord::Schema.define(version: 2018_12_17_093855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2018_12_17_093121) do
     t.string "sku"
     t.string "description"
     t.integer "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shipment_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "shipment_id"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
