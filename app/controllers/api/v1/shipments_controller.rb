bd6b3232a63cdade0ca7c32ba11299d7630aab86
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-12-18 11:14:02 +0100

    filtering results is added

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -3,11 +3,16 @@ module Api
     class ShipmentsController < ApplicationController
 
       def index
-        shipments = Shipment.where(company_id: params[:company_id]).order(ordering)
+        shipments = Shipment.where(company_id: params[:company_id]).order(sorting)
+
+        if !params[:international_transportation_mode].blank?
+          shipments = shipments.where(international_transportation_mode: params[:international_transportation_mode])
+        end
+        
         render json: {records: JsonMaker::Jsonmkr.new.records(shipments)}
       end
 
-      def ordering
+      def sorting
         field = params[:sort].blank? ? 'id' : params[:sort]
         direction = params[:direction].blank? ? 'ASC' : params[:direction]
         "#{field} #{direction}"
