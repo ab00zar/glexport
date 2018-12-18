af6e52a088f16fca4aad8a2057ac466b5dd30a3c
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-12-18 11:05:08 +0100

    ordering method is added

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -3,8 +3,14 @@ module Api
     class ShipmentsController < ApplicationController
 
       def index
-        shipments = Shipment.where(company_id: params[:company_id])
+        shipments = Shipment.where(company_id: params[:company_id]).order(ordering)
         render json: {records: JsonMaker::Jsonmkr.new.records(shipments)}
+      end
+
+      def ordering
+        field = params[:sort].blank? ? 'id' : params[:sort]
+        direction = params[:direction].blank? ? 'ASC' : params[:direction]
+        "#{field} #{direction}"
       end
 
     end
