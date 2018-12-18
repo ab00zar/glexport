9ef202b9084c7e43a0f95715dd9ffca2710acf87
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-12-18 16:03:14 +0100

    refactoring code & adding scopes

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -4,23 +4,17 @@ module Api
 
       def index
         return unprocessable if params[:company_id].blank?
-        shipments = Shipment.where(company_id: params[:company_id]).order(sorting)
 
-        if !params[:international_transportation_mode].blank?
-          shipments = shipments.where(international_transportation_mode: params[:international_transportation_mode])
-        end
+        shipments = Shipment.where(company_id: params[:company_id]).sorting(params[:sort], params[:direction])
 
-        shipments = shipments.page(params[:page]).per(params[:per])
+        shipments = shipments.filters(params[:international_transportation_mode]) if
+            !params[:international_transportation_mode].blank?
+
+        shipments = shipments.pagination(params[:page], params[:per])
 
         render json: {records: JsonMaker::Jsonmkr.new.records(shipments)}
       end
-
-      def sorting
-        field = params[:sort].blank? ? 'id' : params[:sort]
-        direction = params[:direction].blank? ? 'ASC' : params[:direction]
-        "#{field} #{direction}"
-      end
-
+      
       def unprocessable
         render json: {errors: ['company_id is required']}, status: 422
       end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,4 +2,16 @@ class Shipment < ApplicationRecord
   belongs_to :company
   has_many :shipment_products, dependent: :destroy
   has_many :products, through: :shipment_products, dependent: :destroy
+
+  scope :sorting, -> (field,direction) {order(sort_command(field,direction))}
+  scope :filters, -> (mode) {where(international_transportation_mode: mode)}
+  scope :pagination, ->(page, per) {page(page).per(per)}
+
+
+  def self.sort_command(field, direction)
+    field = field.blank? ? 'id' : field
+    direction = direction.blank? ? 'ASC' : direction
+    "#{field} #{direction}"
+  end
+
 end
